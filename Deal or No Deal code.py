f0b0e505104b513787038f48daa6261e865b096d
Addison Huschka <yg0531qj@go.minnstate.edu>
2025-12-04 20:30:57 -0600

    Update Deal or No Deal code.py

--- Deal or No Deal code.py
+++ Deal or No Deal code.py
@@ -7,20 +7,22 @@ class Briefcases:
     def __init__(self):
         self.cases = {} #collection data type 1, dictionary (used to store the number value pairs of the cases)
         self.opened_cases = set() #collection data type 2, set (used to keep track of which cases are open)
+    
     def generate_cases(self): #this generates the cases with random amounts
         amounts = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
         random.shuffle(amounts)
         for number in range(1,27):
             self.cases[number] = amounts[number-1]
+    
     def open_case(self,number): #this opens a case and reveals the amount inside
         if number in self.opened_cases:
             print(f'Case{number} has already been opened.')
         else:
             self.opened_cases.add(number)
             print(f'Case {number} contained ${self.cases[number]:,}')
+    
     def remaining_amounts(self): #this returns the amounts in the unopened cases
-        return [amt for num, amt in self.cases.items() if num not in self.opened_cases] #collection data type 3, list (used to store the remaining amounts in the unopened cases) There are also multiple instances of using lists
-
+        return [amt for num, amt in self.cases.items() if num not in self.opened_cases] #collection data type 3, list (used to store the remaining amounts in the unopened cases)
 
 
 def banker_offer(game): #this is where we calculate the banker's offer
@@ -30,21 +32,18 @@ def banker_offer(game): #this is where we calculate the banker's offer
 #but if someone finds a different way that works too
 
 
-
-def deal_or_no_deal():
+def deal_or_no_deal(): #this asks the player if they want to accept the banker's offer
     choice = input("Do you accept the banker's offer? Deal or no deal?: ").lower()
     return choice == "deal"
 
 
-
-def show_remaining_case_values(game, player_case):
+def show_remaining_case_values(game, player_case): #this shows the remaining case values to the player after each round
     remaining = game.remaining_amounts()
     remaining_sorted = sorted(remaining)
     
     print('\nThe remaining case values are:')
     for value in remaining_sorted:
         print(f'${value:,}')
-
 
 
 def play_round(game, round_number, cases_to_open, player_case): #when called in the game, it will ask to open cases
@@ -106,12 +105,12 @@ def play_the_game(): #main function to play the game
                 break
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 26.")
-    #I did the code above to make sure the player picks a valid case number and so that it is still included in the other calculations and remaining case values. I tested it so trust me that it works (:
+    #Code above is to make sure the player picks a valid case number and so that it is still included in the other calculations and remaining case values
 
     round_number = 0
     end_indicator = False
     
-    while round_number < 9:
+    while round_number < 9: #maximum of 9 rounds
         if round_number < 5:    
             round_structure = [6,5,4,3,2] #number of cases to open each round
             round_number += 1
@@ -137,7 +136,8 @@ def play_the_game(): #main function to play the game
                 break
             else:
                 continue
-    if end_indicator == False:
+            
+    if end_indicator == False: #if the player never accepted a deal from the banker
         for key in game.cases:
             if key not in game.opened_cases and key != player_case:
                 last_case = key
@@ -146,6 +146,6 @@ def play_the_game(): #main function to play the game
         print(f"Congratulations! You won ${game.cases[players_prize]:,.2f}.")
         winnings = game.cases[players_prize]
 
-    create_leaderboard(player_name, winnings)
+    create_leaderboard(player_name, winnings) #update leaderboard at the end of the game
 
-play_the_game()
+play_the_game() #Calls the main function to play the game
